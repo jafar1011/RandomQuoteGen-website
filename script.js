9b28f9451201b84cee8442d65511c559ee6bc45c
Jafar Raheem <gaferr041@gmail.com>
2025-06-16 16:53:04 +0300

    3

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const character = document.getElementById('author');
 const nqButton = document.getElementById('new-quote');
 const tweetLink = document.getElementById('tweet-quote');
 
-const fetchingQuote = function () {
-    fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
-        .then(response => response.json())
-        .then(data => {
-            const quoteObj = data[0];
-            quote.innerHTML = `"${quoteObj.quote}"`;
-            character.innerHTML = `- ${quoteObj.character}`;
+const fetchingQuote = async function () {
+    try {
+        const response = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+        const data = await response.json();
+        const quoteObj = data[0];
 
-            const tweetText = `"${quoteObj.quote}" - ${quoteObj.character}`;
-            const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
-            tweetLink.href = tweetUrl;
-        })
-        .catch(error => {
-            console.error('Error fetching:', error);
-        });
-}
+        quote.innerHTML = `"${quoteObj.quote}"`;
+        character.innerHTML = `- ${quoteObj.character}`;
+
+        const tweetText = `"${quoteObj.quote}" - ${quoteObj.character}`;
+        const tweetUrl = `https://x.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+        tweetLink.href = tweetUrl;
+    } catch (error) {
+        console.error('Error fetching:', error);
+    }
+};
 
 fetchingQuote();
 
